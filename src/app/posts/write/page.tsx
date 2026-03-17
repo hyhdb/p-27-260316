d82eb891e9fbb6fc4f74009d9384fdb5df8c5fe4
hyhdb <kkminwoo@gmail.com>
2026-03-17 15:24:04 +0900

    22-3

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -18,12 +18,24 @@ export default function Write() {
             return;
         }
 
+        if (title.value.length >= 10 || title.value.length < 2) {
+            alert("2글자 이상 10자 미만으로 작성해주세요");
+            title.focus();
+            return;
+        }
+
         if (content.value.length === 0) {
             alert("내용을 입력해주세요.");
             content.focus();
             return;
         }
 
+        if (content.value.length >= 100 || content.value.length < 2) {
+            alert("2글자 이상 100자 미만으로 작성해주세요");
+            content.focus();
+            return;
+        }
+        
         // db에 저장.
         fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/posts`, {
             method: "POST",
