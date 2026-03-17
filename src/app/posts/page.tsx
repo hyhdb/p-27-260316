0eea427e9996915843922229982345a5263d67c6
hyhdb <kkminwoo@gmail.com>
2026-03-17 12:14:44 +0900

    17

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export interface Post {
@@ -28,8 +29,8 @@ export default function Home() {
             : <ul>
                 {posts.map((post) => (
                     <li key={post.id} className="p-2">
-                        {post.id}. {post.title}
-                    </li>
+                        <Link href={`/posts/${post.id}`}>{post.id}. {post.title}</Link>
+                        </li>
                 ))}
             </ul>
     )
